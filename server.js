d80d465f87824a0fe11bc579d87aa30b7936420d
Mike Leung <mpakleung@gmail.com>
2018-02-03 18:01:51 -0500

    Was able to parse the condition of rideau canal

--- server.js
+++ server.js
@@ -6,6 +6,8 @@ var app     = express();
 
 
 
+
+
  app.get('/', function(req, res){
 	console.log("get");
 	res.send({name: "yoshi"});
@@ -14,8 +16,11 @@ var app     = express();
 
 app.get('/scrape', function(req, res){
 
-  	//All the web scraping magic will happen here
-  	
+  	// var title, release, rating;
+   // 	var json = { title : "", length : "", condition : ""};
+
+  	var results = new Array();
+
   	url = 'http://ncc-ccn.gc.ca/places-to-visit/rideau-canal-skateway';
 
 	request(url, function(error, response, html){
@@ -23,13 +28,27 @@ app.get('/scrape', function(req, res){
 		if(!error){
 			var $ = cheerio.load(html);
 			
-			$('.table tbody').filter(function(){
-                var data = $(this);
+			$('.table tbody tr').each(function(i, elem) {
 
-                console.log(data.children().first().text());
-                // title = data.children().first().text();
-                // json.title = title;
-            })
+				var stretch = $(this).text();
+				var rideauArray = stretch.split('\n\t\t\t\t\t\t\t');
+
+			  	results[i] = { title : rideauArray[2], condition : rideauArray[1], length : rideauArray[3]};
+			});
+
+			console.log(results);
+
+			// $('.table tbody').filter(function(){
+   //  //             var data = $(this);
+
+   //  //             data.children().each(function(i, elem) {
+			// 	//   	results.push();
+			// 	// });
+
+   //  //             console.log(data.children().second().text());
+   //              // title = data.children().first().text();
+   //              // json.title = title;
+   //          })
 
 		}
 	});
