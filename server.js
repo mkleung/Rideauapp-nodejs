7cd43fda28694f6bba2cd1567cbc2e489c6f6237
Mike Leung <mpakleung@gmail.com>
2018-02-03 22:46:46 -0500

    Added list api route that reads from the output file

--- server.js
+++ server.js
@@ -6,53 +6,67 @@ var app     = express();
 
 
 
-
-
+/*
+|==========================================================================
+| TEST
+|==========================================================================
+*/
  app.get('/', function(req, res){
 	console.log("get");
-	res.send({name: "yoshi"});
+	res.send({hello: "World"});
 });
 
 
-app.get('/scrape', function(req, res){
+/*
+|==========================================================================
+| GET
+|==========================================================================
+*/
 
-  	// var title, release, rating;
-   // 	var json = { title : "", length : "", condition : ""};
+app.get('/list', function(req, res){
+	var obj = JSON.parse(fs.readFileSync('output.json', 'utf8'));
+	res.send(obj);
+});
+
+
+
+/*
+|==========================================================================
+| SCRAPE
+|==========================================================================
+*/
+app.get('/scrape', function(req, res){
 
   	var results = new Array();
 
   	url = 'http://ncc-ccn.gc.ca/places-to-visit/rideau-canal-skateway';
 
 	request(url, function(error, response, html){
-		
 		if(!error){
 			var $ = cheerio.load(html);
 			
 			$('.table tbody tr').each(function(i, elem) {
-
 				var stretch = $(this).text();
 				var rideauArray = stretch.split('\n\t\t\t\t\t\t\t');
-
 			  	results[i] = { title : rideauArray[2], condition : rideauArray[1], length : rideauArray[3]};
 			});
 
-			console.log(results);
-
-
+			// this saves to an output file
 			fs.writeFile('output.json', JSON.stringify(results, null, 4), function(err){
-
 			    console.log('File successfully written! - Check your project directory for the output.json file');
-
 			});
-
 		}
+
 	});
-
-
 
 });
 
 
+/*
+|==========================================================================
+| START SERVER
+|==========================================================================
+*/
 
 app.listen('8081')
 
