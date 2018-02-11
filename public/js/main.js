563c906ba61704596e7419342573494e75f1461c
Mike Leung <mpakleung@gmail.com>
2018-02-11 16:27:57 -0500

    Added 30% of markers

--- public/js/main.js
+++ public/js/main.js
@@ -17,6 +17,79 @@
 
 /**
 |==========================================================================
+| READ RESULTS FROM JSON FILE
+|==========================================================================
+*/
+
+var locations = [];
+
+$.getJSON( "../rideau.json", function( data ) {
+	var items = [];
+	$.each( data, function( key, val ) {
+		var title = val.title;
+		var condition = val.condition;
+		var length = val.length;
+
+		switch(condition) {
+		    case "Excellent":
+		    	var color = "has-text-info";
+		    	break;
+		    case "Good":
+		        var color = "has-text-success";
+		        break;
+		    case "Fair":
+		        var color = "has-text-primary";
+		        break;
+		    case "Poor":
+		    	var color = "has-text-warning";
+		    	break;
+		    case "Closed":
+		    	var color = "has-text-danger";
+		    	break;
+		    default:
+		        var color = "has-text-danger";
+		}
+
+		items.push( "<li>" + title + " - <span class='" + color + "'><strong>" + condition + "</strong></span></li>" );
+
+		
+		switch(title) {
+		    case "Rideau Locks (NAC) - Mackenzie-King Bridge":
+		    	var latitude = 45.422989;
+		    	var longitude = -75.690186;
+		    	break;
+		    case "MacKenzie-King Bridge - Laurier Bridge":
+		      	var latitude = 45.421755;
+		    	var longitude = -75.686799;
+		        break;
+		    case "Laurier Bridge - Corktown Footbridge / Somerset":
+		        var latitude = 45.420609;
+		    	var longitude = -75.684555;
+		        break;
+		    default:
+		        var latitude = 45.405250;
+		    	var longitude = -75.680992;
+		}
+		
+
+		locations.push([title, latitude , longitude ,  condition]);
+
+	});
+
+$( "<ul/>", {
+		"class": "my-new-list",
+		html: items.join( "" )
+	}).appendTo( "#results" );
+});
+
+    
+
+
+
+
+
+/**
+|==========================================================================
 |  Google maps
 |==========================================================================
 */
@@ -40,12 +113,10 @@ function initMap() {
 // 	visible: true
 // 	});
 
-	var locations = [
-      ['Bondi Beach',45.405250, -75.680992, 4],
-    ];
-
+setTimeout(
+	function() {
     var map = new google.maps.Map(document.getElementById('map'), {
-      zoom: 15,
+      zoom: 13,
       center: new google.maps.LatLng(45.405250, -75.680992),
       mapTypeId: google.maps.MapTypeId.ROADMAP
     });
@@ -59,7 +130,7 @@ function initMap() {
         position: new google.maps.LatLng(locations[i][1], locations[i][2]),
         map: map,
         label: {
-			text: "Fair",
+			text: String(locations[i][3]),
 			color: "#4682B4",
 			fontSize: "10px",
 			fontWeight: "bold"
@@ -68,53 +139,23 @@ function initMap() {
 
       google.maps.event.addListener(marker, 'click', (function(marker, i) {
         return function() {
-          infowindow.setContent(locations[i][0]);
+          infowindow.setContent(locations[i][0] + "(" + locations[i][3] + ")");
           infowindow.open(map, marker);
         }
       })(marker, i));
     }
 
+
+  	}, 500
+);
+
+
+
+	
+
+
 }
 
 
-/**
-|==========================================================================
-| READ RESULTS FROM JSON FILE
-|==========================================================================
-*/
-
-var mapLocations = [];
-
-$.getJSON( "../rideau.json", function( data ) {
-	var items = [];
-	$.each( data, function( key, val ) {
-		var title = val.title;
-		var condition = val.condition;
-		var length = val.length;
-
-		switch(condition) {
-		    case "Excellent":
-		    	var color = "has-text-info";
-		    	break;
-		    case "Good":
-		        var color = "has-text-success";
-		        break;
-		    case "Fair":
-		        var color = "has-text-warning";
-		        break;
-		    default:
-		        var color = "has-text-danger";
-		}
-
-		//mapLocations.push([title, 45.405250, -75.680992, condition, length]);
-
-		items.push( "<li>" + title + " - <span class='" + color + "'><strong>" + condition + "</strong></span></li>" );
-	});
-
-$( "<ul/>", {
-		"class": "my-new-list",
-		html: items.join( "" )
-	}).appendTo( "#results" );
-});
 
 
