ca1162fb352f8cf6a4dda753f56b9e682fc8e417
Mike Leung <mpakleung@gmail.com>
2018-02-11 17:38:04 -0500

    Scrapes every 1 hour

--- server.js
+++ server.js
@@ -87,4 +87,31 @@ app.listen('8081')
 
 console.log('Listening... Go to http://localhost:8081');
 
+
+/*
+|==========================================================================
+| RUN SCRAPER EVERY MINUTE  (60000 = 1 minute)
+|==========================================================================
+*/
+
+var requestLoop = setInterval(function(){
+		
+	  request({
+	      url: "http://localhost:8081/scrape",
+	      method: "GET",
+	      timeout: 10000,
+	      followRedirect: true,
+	      maxRedirects: 10
+	  },function(error, response, body){
+	      if(!error && response.statusCode == 200){
+	          console.log('sucess at scraping!');
+	      }else{
+	          console.log('error' + response.statusCode);
+	      }
+	  });
+}, 3600000);
+
+
 exports = module.exports = app;
+
+
