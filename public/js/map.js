e88b4fdabefd6d5fb9f8fffab6416980c3c60a89
Mike Leung <mpakleung@gmail.com>
2018-02-19 17:50:53 -0500

    progress on displaying all markers

--- public/js/map.js
+++ public/js/map.js
@@ -13,11 +13,9 @@ const RideauMarkerComponent = ({ text }) => (
 );
 
 
-
-
 /**
 |==========================================================================
-| MAP
+| MAP COMPONENT
 |==========================================================================
 */
 class RideauMap extends React.Component {
@@ -39,15 +37,63 @@ class RideauMap extends React.Component {
   }
 
   renderMarkers(map, maps) {
-      let marker = new maps.Marker({
-        position: new google.maps.LatLng(45.405250, -75.680992),
-        map,
-        title: 'Hello World!'
+      // let marker = new maps.Marker({
+      //   position: new google.maps.LatLng(45.405250, -75.680992),
+      //   map,
+      //   label: {
+      //     text: "Poor",
+      //     color: "#000",
+      //     fontSize: "8px",
+      //     fontWeight: "bold"
+      //   },
+      // });
+
+
+
+      var locations = [];
+
+      $.getJSON( "../rideau.json", function( data ) {
+          console.log(data);
       });
+
+      var infowindow = new maps.InfoWindow();
+      var marker, i;
+
+      for (i = 0; i < locations.length; i++) { 
+        marker = new google.maps.Marker({
+          position: new google.maps.LatLng(locations[i][1], locations[i][2]),
+          map: map,
+          label: {
+        text: String(locations[i][3]),
+        color: "#000",
+        fontSize: "12px",
+        fontWeight: "bold"
+      },
+      icon: {
+          path: google.maps.SymbolPath.BACKWARD_CLOSED_ARROW,
+          strokeColor: locations[i][4],
+          scale: 4
+      },
+      });
+
+      google.maps.event.addListener(marker, 'click', (function(marker, i) {
+        return function() {
+          infowindow.setContent(locations[i][0] + "(" + locations[i][3] + ")" +  "<p>" + locations[i][5] + "</p>");
+          infowindow.open(map, marker);
+        }
+      })(marker, i));
     }
 
+
+    }
 }
 
+
+/**
+|==========================================================================
+| RENDER
+|==========================================================================
+*/
 
 ReactDOM.render(
   <div style={{width: '100%', height: '400px'}}>
